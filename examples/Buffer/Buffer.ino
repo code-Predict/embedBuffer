e23394a6a2855f88adaead5bfac608864a5ab677
Enchan <enchant0130@gmail.com>
2020-11-08 22:56:18 +0900

    [Fix] buffer example source

--- examples/Buffer/Buffer.ino
+++ examples/Buffer/Buffer.ino
@@ -9,15 +9,16 @@ void setup(){
     Serial.begin(115200);
 
     // バッファ初期化
+    unsigned int bufferSize = 100;
     B = &buffer;
-    initBuffer(B);
+    initBuffer(B, bufferSize);
 }
 
 void loop(){
     // バッファサイズ+20回push
     Serial.println("Pushing...");
     Item pushItem;
-    for (int i = 0; i < buffer.length + 20; i++) {
+    for (int i = 0; i < buffer.size + 20; i++) {
         pushItem.id = i;
         push(B, pushItem);
         Serial.println(pushItem.id);
